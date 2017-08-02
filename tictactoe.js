741f1cc25263722f7fa0f54c081ef01cf1c085ba
Almada Escartin <jalmada@cgi.clayton.com>
2017-08-02 10:11:42 -0400

    Separated into files

---
+++ tictactoe.js
@@ -0,0 +1,196 @@
+    Circle = function(x,y,radius, lineWidth){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.lineWidth = lineWidth || 10;
+        this.offset = this.lineWidth * 1.2;
+        
+
+        this.paint = (function(ctx){
+            ctx.strokeStyle = "rgb(0,0,0)";
+            ctx.lineWidth=this.lineWidth;
+            ctx.beginPath();
+            ctx.arc(this.x + this.radius, this.y + this.radius, this.radius - this.offset, 0,Math.PI + (Math.PI * 2) / 2, true)
+            ctx.stroke();
+        }).bind(this);
+    }
+
+    Cross = function(x,y,width, height, lineWidth){
+        this.lineWidth = lineWidth || 10;            
+        this.offset = this.lineWidth * 1.2;            
+        this.x = x + this.offset;
+        this.y = y + this.offset;
+        this.width = width - (this.offset * 2);
+        this.height = height - (this.offset * 2);
+        
+
+        this.paint = (function(ctx){
+            ctx.strokeStyle = "rgb(0,0,0)";
+            ctx.lineWidth=this.lineWidth;
+            ctx.beginPath();
+            ctx.moveTo(this.x,this.y);
+            ctx.lineTo(this.x + this.width,this.y + this.height);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.moveTo(this.x + this.width,this.y);
+            ctx.lineTo(this.x, this.y + this.height);
+            ctx.stroke();
+        }).bind(this);
+
+    }
+
+    Mesh = function(cellsx, cellsy, width, height, lineWidth){
+        this.cellsx = cellsx;
+        this.cellsy = cellsy;
+        this.width = width;
+        this.height = height;
+        this.lineWidth = lineWidth || 5;
+
+        this.countVLines = cellsx - 1;
+        this.countHLines = cellsy - 1;
+        this.cellwidth = Math.ceil(width/cellsx);
+        this.cellheight = Math.ceil(height/cellsy);
+
+        this.paint = (function(ctx){
+            for(var x = 0, xpos = this.cellwidth; x < this.countVLines; x++, xpos = xpos + this.cellwidth){
+                ctx.strokeStyle = "rgb(0,0,0)";
+                ctx.lineWidth=this.lineWidth;
+                ctx.beginPath();
+                ctx.moveTo(xpos,5);
+                ctx.lineTo(xpos, this.height - 5);
+                ctx.stroke();
+            }
+
+            for(var y = 0, ypos = this.cellheight; y < this.countHLines; y++, ypos = ypos + this.cellheight){
+                ctx.strokeStyle = "rgb(0,0,0)";
+                ctx.lineWidth=this.lineWidth;
+                ctx.beginPath();
+                ctx.moveTo(5,ypos);
+                ctx.lineTo(this.width - 5, ypos);
+                ctx.stroke();
+            }           
+        }).bind(this);  
+    }
+
+    TicTacToe = function(canvasId, xcells, ycells){
+
+            this.canvasId = canvasId;
+            this.canvas = document.getElementById(this.canvasId);
+
+            this.ctx = this.canvas.getContext('2d');;
+
+            this.xcells = xcells;
+            this.ycells = ycells;
+            
+            this.elementWidth = Math.ceil(this.canvas.width / this.xcells);
+            this.elementHeight = Math.ceil(this.canvas.height / this.ycells);
+            this.elementRadius = Math.ceil((this.canvas.width / this.xcells)/2);
+
+            this.currentShape = 1;
+            this.shapeLimit = this.xcells * this.ycells;
+            this.shapeCount = 0;
+
+            this.spaces = [];
+            this.lineWidth = ((this.canvas.width + this.canvas.height) / 2) * .02 ;
+
+            this.getCellToUse = (function(v, q){
+                var i = q;
+                var p = 0
+                while( i < v){
+                    p++;
+                    i = i + q;
+                }
+                return p;
+            }).bind(this);
+
+
+            this.onCanvasClick = (function(e){
+                if(this.shapeCount > this.shapeLimit){
+                    this.reset();
+                    return;
+                }
+
+                if(this.shapeCount == this.shapeLimit){
+
+                    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                    this.ctx.font = "30px Arial";
+                    this.ctx.textAlign="center"; 
+                    this.ctx.fillText("Game Over",this.canvas.width/2,(this.canvas.height/2)-15);
+                    this.ctx.font = "15px Arial";                    
+                    this.ctx.fillText("Click to try Again",this.canvas.width/2,(this.canvas.height/2)+5);
+
+                    this.shapeCount++;
+                    return;
+                }
+
+                var pos = this.getMousePos(e);
+
+                var cposx = this.getCellToUse(pos.x, this.elementWidth);
+                var cposy = this.getCellToUse(pos.y, this.elementHeight);
+
+                var placeX = cposx * this.elementWidth;
+                var placeY = cposy * this.elementHeight;
+
+                if(!this.spaces[cposx][cposy]){
+                    this.drawNextShape(placeX, placeY);
+                    this.spaces[cposx][cposy] = true;
+                }
+            }).bind(this);
+
+            this.initSpaces = (function(){
+                this.spaces = [];
+                for(var x = 0; x < this.xcells; x++){
+                    this.spaces.push([]);
+                    for(var y = 0; y <this.ycells; y++){
+                        this.spaces[x].push(false);
+                    }
+                }
+            }).bind(this);
+
+            this.draw = (function() {
+                if (this.canvas.getContext) {
+                    
+                    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                    this.initSpaces();
+                    this.currentShape = 1;
+                    this.shapeCount = 0;
+
+                    var mesh = new Mesh(this.xcells,this.ycells,this.canvas.width,this.canvas.height, this.lineWidth / 2);
+                    mesh.paint(this.ctx);
+                }
+            }).bind(this);
+
+            this.getMousePos = (function(evt) {
+                var rect = this.canvas.getBoundingClientRect();
+                return {
+                    x: evt.clientX - rect.left,
+                    y: evt.clientY - rect.top
+                };
+            }).bind(this);        
+
+            this.drawNextShape = (function(x,y){
+                
+                if(this.currentShape == 1){
+                    var cross = new Cross(x,y, this.elementWidth, this.elementHeight, this.lineWidth);
+                    cross.paint(this.ctx);
+                    this.currentShape = 0;
+                } else {
+                    var circle = new Circle(x, y , this.elementRadius, this.lineWidth);
+                    circle.paint(this.ctx);
+                    this.currentShape = 1;                
+                }
+                this.shapeCount++;
+            }).bind(this);
+
+            this.reset = (function(){
+                this.draw();
+            }).bind(this);
+
+            this.canvas.addEventListener('click', this.onCanvasClick, false);
+            document.getElementById("reset").onclick = this.reset;
+            
+            return {
+                draw: this.draw
+            }
+    }
