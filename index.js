8a117cbf2f91679f0a7555086df39b6b541180ae
Almada Escartin <jalmada@cgi.clayton.com>
2017-09-07 15:49:35 -0400

    Added Webpack config for css

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import TicTacToe from './tictactoe.js';
+import './style.css'
 
 var ticTacToe = new TicTacToe("tictactoe", 3, 3);
 ticTacToe.draw();
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.js',
+  entry: path.resolve(__dirname,'index.js'),
   output: {
-    path: '/dev/33across',
+    path: path.resolve(__dirname, 'dist'),
     filename: 'index-bundle.js'
   },
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ],
+    rules: [
+      { test: /\.css$/, use: ['style-loader','css-loader'] }
     ]
   }
 }
