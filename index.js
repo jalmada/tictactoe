6ea47876a2eaba384d97921cf4bacee6d63370a0
Jose L. Almada <jalmada.e@gmail.com>
2017-09-22 01:13:35 -0500

    Fix new name

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import TicTacToePro from './tictactoepro.js';
 import './style.css'
 
-var ticTacToe = new TicTacToe("tictactoe", 3, 3)
+var ticTacToe = new TicTacToePro("tictactoe", 3, 3)
 ticTacToe.draw();
